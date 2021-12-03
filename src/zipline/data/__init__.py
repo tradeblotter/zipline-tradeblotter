f7a71a0a117c12c4a199025031cbc11e73aeec6d
Jason <strimp101@gmail.com>
2021-12-04 07:08:59 +0800

    Add code from prior versions of zipline

--- src/zipline/data/__init__.py
+++ src/zipline/data/__init__.py
@@ -2,11 +2,13 @@ from . import loader
 from .loader import (
     load_prices_from_csv,
     load_prices_from_csv_folder,
+    get_data_filepath
 )
 
 
 __all__ = [
     "load_prices_from_csv",
     "load_prices_from_csv_folder",
+    "get_data_filepath",
     "loader",
 ]
