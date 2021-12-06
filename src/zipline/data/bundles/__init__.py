6f3c991e623da0a9010aa3759b0c944e4535fedb
Jason <strimp101@gmail.com>
2021-12-07 07:08:19 +0800

    Add quandl_eod

--- src/zipline/data/bundles/__init__.py
+++ src/zipline/data/bundles/__init__.py
@@ -1,6 +1,6 @@
 # These imports are necessary to force module-scope register calls to happen.
-from . import quandl_eod  # noqa
 from . import quandl  # noqa
+from . import quandl_eod  # noqa
 from . import csvdir  # noqa
 
 from .core import (
